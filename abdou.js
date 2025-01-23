bb1028bd929e6c6ffc8c75d9f9a1b3b4c1666491
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-01-22 20:13:24 -0500

    adding the array of greeting

--- abdou.js
+++ abdou.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const myarray = [
+"hello, mr.jordan",
+"Welcome to the main page",
+"stay positive"
+
+]
+
+
+
 
 app.get("/", (req, res) => {
   res.send("welcome to the main page");
 });
+
 
 app.listen(PORT, () => {
   console.log("hello abdou");

617765a20d8d28cd587d7ecd466c1648ebad8f8c
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-01-22 20:22:35 -0500

    adding the first GET methode

--- abdou.js
+++ abdou.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 const myarray = [
-"hello, mr.jordan",
-"Welcome to the main page",
-"stay positive"
+  "hello, mr.jordan",
+  "Welcome to the main page",
+  "stay positive",
+];
 
-]
-
-
-
-
+// app.get("/", (req, res) => {
+//   res.send("welcome to the main page");
+// });
 app.get("/", (req, res) => {
-  res.send("welcome to the main page");
+  const random = myarray[Math.floor(Math.random() * myarray.length)];
+  res.send(`<h1>${random}</h1>`)
 });
-
 
 app.listen(PORT, () => {
   console.log("hello abdou");

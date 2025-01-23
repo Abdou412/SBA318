cc05bc1e4b1a7dcfce8daaa7215d0b61ccf914c5
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-01-22 21:54:16 -0500

    add the error and middleware

--- abdou.js
+++ abdou.js
@@ -3,17 +3,27 @@ const app = express();
 const PORT = 3000;
 const myarray = [
   "hello, mr.jordan",
-  "Welcome to the main page",
   "stay positive",
+  "you are a great man and wonderful instructor",
 ];
-
-// app.get("/", (req, res) => {
-//   res.send("welcome to the main page");
-// });
+function mytest(req, res, next) {
+  console.log("you are very welcome");
+  next();
+}
+app.use(mytest);
 app.get("/", (req, res) => {
-  const random = myarray[Math.floor(Math.random() * myarray.length)];
-  res.send(`<h1>${random}</h1>`)
+  res.send(`<h1>WELCOM THE THE MAIN PAGE</h1>`);
 });
+app.get("/grt", (req, res) => {
+  const random = myarray[Math.floor(Math.random() * myarray.length)];
+  res.send(`<h1>${random}</h1>`);
+});
+
+app.use((err, rer, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("you made a mistake");
+});
+
 
 app.listen(PORT, () => {
   console.log("hello abdou");
